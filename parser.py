7800713660302652e4f668f02e209cf640e033e8
Sauli <sauli.flinkman@gmail.com>
2015-10-06 13:38:16 +0300

    pikkumuutoksia

---
+++ parser.py
@@ -0,0 +1,51 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import urllib, cStringIO
+
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+#Twitter API:n tarvitsemat tiedot
+ACCESS_TOKEN = '3869893516-1jWdaC3do0Dvhyb8TP8C6bvaYPRBMt28ug4aoDW'
+ACCESS_SECRET = 'IbEu7Q0an9E3T3wzzrYCIRKqy9T370Xnz4HdHFTcrEfWI'
+CONSUMER_KEY = 'kwZuZArqeNhEtP5iFZLbA4tGq'
+CONSUMER_SECRET = 'AxHMUj8Am1Z0bTrjg7OlZmIW6S1iCCbCGJj7esvNwsWOkFCxVM'
+COUNT = 1
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter = Twitter(auth=oauth)
+query = twitter.search.tweets(q='#jyvaeskylae', result_type='recent', count=COUNT)
+
+img_urls = []
+tweets = [] #sisakkainen lista [[kayttaja, teksti, kuvan url tai '']]
+x = 0
+while x < COUNT:
+   user = query['statuses'][x]['user']['screen_name']  #tweettaajan kayttajanimi
+   text = query['statuses'][x]['text'] #tweetin teksti (escaped)
+   url = ''
+
+   try:
+     url  = query['statuses'][x]['entities']['media'][0]['media_url'] #tweetissa olevan kuvan url
+     #print url
+     img_urls.append(url)
+     tweets.append([user,text,url])
+   except:
+      tweets.append([user,text,""])
+      x += 1
+      continue 
+   x += 1
+
+
+print tweets #testi
+print tweets[0][0] #testi. kayttajanimen tulostus
+
+#hakee kuvat URL-osoitteiden perusteella
+def imagesFromURL(urls):
+   images = []
+   for url in urls:
+      file = cStringIO.StringIO(urllib.urlopen(URL).read())
+      img = Image.open(file)
+      images.append(img)
+   return images
+
