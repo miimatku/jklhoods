bc46cc2b1f966188e92557057fad3c45454e1abb
Sauli Flinkman <Sauli Flinkman@gmail.com>
2015-11-10 18:32:42 +0200

    yhdistetty instagram paaohjelmaan

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from flask import render_template
-from jinja2 import Template
-import twiitit
+import multiprocessing
+import streaminstagram
+import time, sys
+
+"""
+https://api.instagram.com/v1/subscriptions?client_secret
+=bdadba8a4b274b45bdfcb306cfd6b120&client_id=efe6cccbd3ac4e75b842c957e954c569
+"""
+
 app = Flask(__name__)
+app.add_url_rule('/realtime', methods = ['GET', 'POST'], view_func=streaminstagram.callback)
+
 
 @app.route('/')
 def index():
@@ -11,10 +19,46 @@ testimuuttuja='testi2')
 @app.route('/twitter')
 def twitter():
     return 'Twitter is here'
-
 @app.route('/about')
 def about():
-    return 'The about page'
+    return 'The about page'    
+    
+class ApplicationProcess(multiprocessing.Process):
 
+    def __init__(self, ):
+        multiprocessing.Process.__init__(self)
+        self.exit = multiprocessing.Event()
+
+    def run(self):
+        try:
+            startApp()
+        except (KeyboardInterrupt, SystemExit):
+            print "Exiting..."
+
+    def terminate(self):
+        print "Flask application shutdown initiated"
+        self.exit.set()
+    
+    
+def startApp():
+    global app
+    app.run(debug=True, port=8000, use_reloader=True)
+
+
+def initializeInstagram():
+    streaminstagram.startSubscription()    
+    
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+   flaskapp = ApplicationProcess()
+   flaskapp.start()
+   instagramSubscription = multiprocessing.Process(target=initializeInstagram)
+   instagramSubscription.start()
+   
+   while True:
+      try:
+        time.sleep(1)
+      except KeyboardInterrupt, SystemExit:
+        instagramSubscription.terminate()
+        flaskapp.terminate()
+        sys.exit(0)
