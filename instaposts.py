f6d95fc8d858a83f64665ab72a2085bbf0551dbf
Sauli <sauli.flinkman@gmail.com>
2015-11-24 14:37:31 +0200

    tagin mukaan hakeminen

--- instaposts.py
+++ instaposts.py
@@ -1,6 +1,7 @@
 import sqlite3 as lite
 import sys
 
+
 def instagramPosts():
 	con = None
 
@@ -25,5 +26,13 @@ def instagramPosts():
 			con.close()
 
 
-def fetchLatest():
+def fetchWithTag(tag):
+	try:
+		con = lite.connect('instagram.db')
+		data = []
+		cur = con.cursor()
+		cur.execute('SELECT shortcode FROM instagram_posts,instagram_tags WHERE instagram_posts.mediaID=instagram_tags.mediaID AND hashtag LIKE ? LIMIT 10', (tag,) )
+		rows = cur.fetchall()
+	except Exception, e:
+		print "Error &s:" % e.args[0]
 	return
